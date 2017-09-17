464e473039c7fda09619cc8552bb1b8c449d1298
htdong <ht.dong@yahoo.com.vn>
2017-09-17 15:52:25 +0700

    GK|BPS Start
    GK|BPS Backend project start
    Todo
    - gkClient Management
    - Services Management
    - User Management

---
+++ src/services/sequenceIndex.service.ts
@@ -0,0 +1,56 @@
+// External
+import express = require("express");
+var mongoose = require("mongoose");
+mongoose.Promise = global. Promise;
+var Schema = mongoose.Schema;
+
+// Internal
+var ConstantsBase = require('../config/base/constants.base');
+import  { SimpleHash } from './simpleHash.service';
+
+// Schema
+var CounterSchema = new Schema ({
+  _id: { type: String, required: true, unique: true },
+  seq: { type: Number, required: true },
+  status: { type: String },
+  created_at: Date,
+  updated_at: Date,
+}, { collection: 'counters' });
+
+CounterSchema.pre('save', (next) => {
+  let currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
+});
+
+export class SequenceIndex {
+  db;
+  counterName;
+
+  constructor (
+    db: any,
+    counterName: string,
+  ) {
+    this.counterName = counterName;
+    this.db = db;
+  }
+
+  public getNextSequence() {
+
+    var criteria = { _id: this.counterName };
+    var sort =  {$natural: 1};
+    var update =  { $inc: { seq: 1 } };
+    var options = {
+      remove: false,
+      new: true,
+      upsert: true
+    };
+
+    var Counter = this.db.model('counters', CounterSchema);
+    return Counter.findAndModify(criteria, sort, update, options);
+  }
+
+}
